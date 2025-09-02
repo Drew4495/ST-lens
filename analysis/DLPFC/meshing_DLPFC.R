05500c3353551d2ec02fd1b21d2fd837c36a7ea4
Drew Burns <drew4495@gmail.com>
2025-09-02 13:54:23 -0500

    Committing all DLPFC scripts for manuscript submission

---
+++ analysis/DLPFC/meshing_DLPFC.R
@@ -0,0 +1,249 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# %% DLPFC dataset mesh generation %%
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+rm(list = ls())
+graphics.off()
+
+# libraries ----
+suppressMessages(library(sf))
+suppressMessages(library(ggplot2))
+suppressMessages(library(dplyr))
+suppressMessages(library(raster))
+suppressMessages(library(rmapshaper))
+suppressMessages(library(fdaPDE))
+
+# sources ----
+source("src/utils/cat.R")
+source("src/utils/directories.R")
+source("src/utils/plots.R")
+source("src/meshing.R")
+source("src/plots.R")
+
+
+
+#=============================================================================#
+
+
+
+cat.script_title("DLPFC dataset mesh generation")
+
+
+# global variables ----
+
+cat.section_title("Global variables")
+
+name_dataset <- "DLPFC"
+
+## paths
+path_data <- paste("data/", name_dataset, "/", sep = "")
+path_images <- paste("images/", name_dataset, "/", sep = "")
+mkdir(c(path_images))
+path_images <- paste("images/", name_dataset, "/meshing/", sep = "")
+mkdir(c(path_images))
+
+## figures dimensions
+figure_width <- 7.5
+figure_height <- 8
+
+
+
+#=============================================================================#
+
+
+
+# data ----
+
+cat.section_title("Data")
+
+## load data
+load(paste(path_data, "data.RData", sep = ""))
+rm(counts)
+
+# Removing NaN
+any(is.na(true_labels$true_label))
+locations <- locations[!is.na(true_labels$true_label),]
+
+## create sp object
+locations <- SpatialPoints(locations)
+
+# range(locations@coords[,1])
+xmin <- 125
+xmax <- 510
+# range(locations@coords[,2])
+ymin <- -515
+ymax <- -110
+
+## plot
+plot <- ggplot() +
+  xlim(xmin, xmax) + ylim(ymin, ymax) + mesh_generation_plot_settings() + 
+  geom_sf(data = st_as_sf(locations), color = "black", size = 0.1)
+ggsave(paste(path_images, "0_initial_locations.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+
+
+#=============================================================================#
+
+
+
+# domain identification ----
+
+cat.section_title("Domain identification")
+
+
+## grid ----
+
+cat.subsection_title("Grid generation")
+
+## lattice type
+type = "hexagonal"
+
+## step of the grid
+h <- 6.4
+
+## grid seed Point
+seed_point <- SpatialPoints(data.frame(x = 235, y = -126))
+
+## plot
+plot <- ggplot() + mesh_generation_plot_settings() +
+  xlim(xmin, xmax) + ylim(ymin, ymax) +
+  geom_sf(data = st_as_sf(locations), color = "black", size = 0.1) +
+  geom_sf(data = st_as_sf(seed_point), color = "red", size = 0.15) +
+  geom_sf(data = st_as_sf(hex(seed_point, h/sqrt(3))), fill = "blue", alpha = 0.5, color = "black")
+ggsave(paste(path_images, "1_initial_locations_check.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+## grid generation
+grid <- generate_grid(locations@bbox, h, seed_point, type = type)
+
+## plot
+plot <- ggplot() + mesh_generation_plot_settings() +
+  xlim(xmin, xmax) + ylim(ymin, ymax) +
+  geom_sf(data = st_as_sf(grid), color = "blue", size = 0.05) +
+  geom_sf(data = st_as_sf(seed_point), color = "red", size = 1)
+ggsave(paste(path_images, "2_grid.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+
+## lattice ----
+
+cat.subsection_title("Lattice generation")
+
+## select polygons to keep
+check <- c()
+polygons <- list()
+polygons_all <- list()
+for(i in 1:nrow(grid@coords)){
+  point <- grid[i,]
+  polygon <- hex(point, h/sqrt(3) - 1e-9)
+  check[i] <- any(!is.na(over(locations, polygon)))
+  if(check[i]){
+    polygons <- c(polygons, polygon@polygons[[1]]@Polygons[[1]])
+  }
+  polygons_all <- c(polygons_all, polygon@polygons[[1]]@Polygons[[1]])
+}
+lattice <- SpatialPolygons(list(Polygons(polygons, ID = "hex_lattice")))
+lattice_all <- SpatialPolygons(list(Polygons(polygons_all, ID = "hex_lattice")))
+
+## plot all
+plot <- ggplot() + mesh_generation_plot_settings() +
+  xlim(xmin, xmax) + ylim(ymin, ymax) +
+  geom_sf(data = st_as_sf(lattice_all), fill = "blue", alpha = 0.5, color = "black")
+ggsave(paste(path_images, "3_lattice.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+## plot all and locations
+plot <- ggplot() + mesh_generation_plot_settings() +
+  xlim(xmin, xmax) + ylim(ymin, ymax) +
+  geom_sf(data = st_as_sf(lattice_all), fill = "blue", alpha = 0.5, color = "black") +
+  geom_sf(data = st_as_sf(locations), color = "black", size = 0.1)
+ggsave(paste(path_images, "4_lattice_and_locations.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+## plot selected
+plot <- ggplot() + mesh_generation_plot_settings() +
+  xlim(xmin, xmax) + ylim(ymin, ymax) +
+  geom_sf(data = st_as_sf(lattice), fill = "blue", alpha = 0.5, color = "black")
+ggsave(paste(path_images, "5_lattice_only_selected.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+
+## simplification ----
+## |||||||||||||||||||
+
+cat.subsection_title("Domain simplification")
+
+## percentage of boundary vertices to be kept
+simplification <- 0.25
+
+## holes parameters
+remove_holes <- TRUE
+minimum_area_hole <- NULL
+simplification_hole <- NULL
+
+## lattice
+lattice <- generate_lattice(locations, h, locations@bbox, seed_point, type = type)
+
+## plot
+plot <- ggplot() + mesh_generation_plot_settings() +
+  xlim(xmin, xmax) + ylim(ymin, ymax) +
+  geom_sf(data = st_as_sf(lattice$domain), fill = "grey", alpha = 0.5, color = "black")
+ggsave(paste(path_images, "6_domain.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+## domain simplification
+lattice_simplified <- simplify_domain(lattice, simplification,
+                                      remove_holes, minimum_area_hole, simplification_hole)
+
+## plot
+plot <- plot.discretized_domain(lattice_simplified, size = 0.1) +
+  mesh_generation_plot_settings() +
+  xlim(xmin, xmax) + ylim(ymin, ymax) + guides(color = "none")
+ggsave(paste(path_images, "7_domain_simplified.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+## discarded points
+indexes.discarded_locations <- is.na(over(locations, lattice_simplified$domain))
+locations.final <- locations[!indexes.discarded_locations,]
+
+## plot
+plot <- plot.final_locations(locations, SpatialPoints(locations.final), lattice_simplified, size = 0.05) +
+  mesh_generation_plot_settings() +
+  xlim(xmin, xmax) + ylim(ymin, ymax)
+ggsave(paste(path_images, "8_final_locations.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+
+
+#=============================================================================#
+
+
+
+# meshing ----
+
+cat.section_title("Meshing")
+
+## maximum area for a mesh element
+maximum_area <- 120
+
+## mesh generation
+mesh <- generate_mesh(lattice_simplified, maximum_area)
+
+## plot
+plot <- plot.fdaPDE_mesh(mesh) +
+  mesh_generation_plot_settings() +
+  xlim(xmin, xmax) + ylim(ymin, ymax)
+ggsave(paste(path_images, "9_mesh.pdf", sep = ""),
+       plot = plot, width = figure_width, height = figure_height, dpi = "print", units = "cm")
+
+
+
+#=============================================================================#
+
+
+
+# saving results ----
+lattice <- lattice_simplified
+save(mesh, lattice,
+     file = paste(path_data, "mesh.RData", sep = ""))
